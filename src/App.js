a05d53c2733e11830b57cfecb00de0198640fe9e
nmaglio <maurofallico@gmail.com>
2025-06-06 13:07:25 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [inputCalle, setInputCalle] = useState('')
+  const [results, setResults] = useState();
+  const [errorMessage, setErrorMessage] = useState('');
+
+  function getDireccion(calle) {
+    setResults();
+    axios.get(`${process.env.REACT_APP_API_URL}?direccion=${calle}`).then((res) => {
+      if (res.data.direccionesNormalizadas.length > 0){
+        setResults(res.data.direccionesNormalizadas);
+        setErrorMessage('');
+        setInputCalle('');
+      }
+      else{
+        setErrorMessage(res.data.errorMessage);
+      }
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>EJERCICIO TÉCNICO - CALLES</h1>
+      <div className="Container">     
+        <div className="Input">
+            <input value={inputCalle} onChange={(e) => setInputCalle(e.target.value)}/>
+            <button className="Button" disabled={!inputCalle} onClick={() => getDireccion(inputCalle)}>CONSULTAR</button>
+        </div>
+      <div className="ResultsContainer">
+        {results?.map((res, index) => {
+          return (
+            <div key={index} >
+              {res.nombre_localidad === 'CABA' ? (
+                <div className="Result">
+                  <div>
+                    <span>Calle: </span>
+                    <span>{res.nombre_calle}</span>
+                  </div>
+                  <div>
+                    <span>Dirección: </span>
+                    <span>{res.direccion}</span>
+                  </div>
+                </div>
+              ) : (null)}
+            </div>
+          )
+        })}
+        </div>
+        <span>{errorMessage}</span>
+      </div>
     </div>
   );
 }
+
 
 export default App;
